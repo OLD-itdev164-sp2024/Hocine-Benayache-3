58941cc502f01ee24715baa883554c47303c876b
Hocineho <112357323+Hocineho@users.noreply.github.com>
2024-04-07 14:38:01 +0100

    Initialize activity-2

---
+++ blog/gatsby-config.js
@@ -0,0 +1,55 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Blog`,
+    description: ``,
+    author: `Mike Hunsicker`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Mike Hunsicker`,
+      company: `Blogs Inc.`,
+      address: `PO Box 1234`
+    }
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `c9bm8f026qbv`,
+        accessToken: `d48c122145adaca03b89ab64b049d2ce48889ef4a4ed766f2d9e2e4c3d697869`
+      }
+    },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
